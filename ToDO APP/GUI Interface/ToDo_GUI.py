040315bc2d3ef51f1e6376f7a0d910c1e7fac855
Abdallah Hossam-Eldin Hosni <eng.abdallahhossam@outlook.com>
2024-01-17 17:44:32 +0200

    The GUI Version of the ToDo is Done

---
+++ ToDO APP/GUI Interface/ToDo_GUI.py
@@ -0,0 +1,61 @@
+import PySimpleGUI as sg
+import functions
+
+sg.theme("DarkBlue")
+addButton = sg.Button("Add", size= 13, mouseover_colors="Blue")
+inputBox = sg.Input(tooltip = "Enter the ToDo", key= "Add Text Box")
+label = sg.Text("Type in the ToDo")
+
+listBox = sg.Listbox(values= functions.getToDos("todos.txt"),
+                     key= "ToDo List",
+                     enable_events= True,
+                     size = [45, 10])
+editButton = sg.Button("Edit", mouseover_colors="Yellow")
+
+completeButton = sg.Button("Complete", mouseover_colors="Green")
+
+exitButton = sg.Button("Exit")
+
+window = sg.Window("My ToDO App", 
+                   layout= [[label],
+                            [inputBox, addButton], 
+                            [listBox, editButton, completeButton],
+                            [exitButton]],
+                   font= ('Helvetica', 14))
+while True:
+    event , values = window.read() # * this is called structure binding in C++ -Multiple Values Assignment-
+    
+    match event:
+        case "Add":
+            todos = functions.getToDos("todos.txt")
+            todos.append(values["Add Text Box"] + "\n")
+            functions.writeToDos("todos.txt", todos)
+            window['ToDo List'].update(todos)
+        case "Edit":
+            try:
+                todoOld = values['ToDo List'][0] 
+                todoNew = values['Add Text Box']+ '\n'
+
+                todos = functions.getToDos("todos.txt")
+                todos[todos.index(todoOld)] = todoNew
+                functions.writeToDos("todos.txt", todos)
+                window['ToDo List'].update(values= todos)
+            except IndexError:
+                sg.popup("Please, Select the ToDo you want to edit first.", text_color= "Red", title= "Error!!")
+        case 'ToDo List':
+            window['Add Text Box'].Update(values['ToDo List'][0])
+        case "Complete":
+            try:
+                completedTodo = values['ToDo List'][0]
+                todos = functions.getToDos("todos.txt")
+                todos.remove(completedTodo)
+                functions.writeToDos("todos.txt", todos)
+                window['ToDo List'].update(values= todos)
+                window["Add Text Box"].update(value= '')
+            except IndexError:
+                sg.popup("Please, Select the ToDo you want to complete first.", text_color= "Red", title= "Error!!")
+        case sg.WIN_CLOSED | "Exit":
+            break
+
+window.close()
+
